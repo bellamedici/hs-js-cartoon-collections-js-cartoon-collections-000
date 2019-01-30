28937e694fb983d3733367eb5e070c1fc1258e90
Isabella de Medici <bellamedici242@gmail.com>
2019-01-30 22:54:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(names) {
+     var returnString = ""
+     for(var i = 0; i < names.length; i++) {
+        var subString = `${i + 1}. ${names[i]} `
+        returnString += subString
+      }
+  return returnString;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    var upperCaseString = planeteerCalls[i].toUpperCase();
+    var finalString= upperCaseString + "!";
+  arr.push(finalString);
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
-}
+  var returnStatement = false;
+  for (var i = 0; i < words.length; i++) {
+     if(words[i].length > 4) {
+     returnStatement = true;
+     }
+  }
+  return returnStatement;
+  }
 
 function findTheCheese (foods) {
+  var cheeseArray = ["cheddar", "gouda", "camembert"];
+    for(var i = 0; i < foods.length; i++) {
+      if(cheeseArray.includes(foods[i])) {
+        return foods[i];
+       } 
+      
+    }
+     return "no cheese!"
 }
